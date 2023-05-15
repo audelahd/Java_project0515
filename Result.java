86aef875098cba0045d8f5c8714b9076c058903e
myeong <audelahd@gmail.com>
2023-05-15 16:45:10 +0900

    read 메소드로 DB에서 데이터 불러와서 게시글 내용 조회

--- Result.java
+++ Result.java
@@ -125,7 +125,43 @@ public class Result {
 			list();
 		}
 		public void read() {
-			System.out.println(" *** read() 메소드 실행됨");
+			System.out.println("[게시물 읽기");
+			System.out.print("bno : ");
+			int bno = Integer.parseInt(scanner.nextLine());
+			
+			try {
+				String sql = ""+
+			"SELECT bno, btitle, bcontent, bwriter, bdate "+
+						"FROM boards "+ "WHERE bno=?";
+				
+				PreparedStatement pstmt = conn.prepareStatement(sql);
+				pstmt.setInt(1,bno);
+				
+				ResultSet rs = pstmt.executeQuery();
+				
+				if(rs.next()) {
+					Board board = new Board();
+					board.setBno(rs.getInt("bno"));
+					board.setBtitle(rs.getString("btitle"));
+					board.setBcontent(rs.getString("bcontent"));
+					board.setBwriter(rs.getString("bwriter"));
+					board.setBdate(rs.getDate("bdate"));
+					
+					System.out.println("#############");
+					System.out.println("번호 : "+board.getBno());
+					System.out.println("제목 : "+board.getBtitle());
+					System.out.println("내용 : "+board.getBcontent());
+					System.out.println("작성자 : "+board.getBwriter());
+					System.out.println("날짜 : "+board.getBdate());
+					System.out.println("#############");
+				}
+				rs.close();
+				pstmt.close();
+				
+			}catch (Exception e) {
+				e.printStackTrace();
+				exit();
+			}
 			list();
 		}
 		public void clear() {
